1c695a5402266b899de9ad38b554019578aec6d7
Carlos Hernández <cxhernandez@users.noreply.github.com>
2015-12-23 12:12:28 -0500

    Update amd_sdk.sh

--- amd_sdk.sh
+++ amd_sdk.sh
@@ -7,7 +7,7 @@ fi
 
 # Location from which to download
 url1="http://developer.amd.com/tools-and-sdks/opencl-zone/opencl-tools-sdks/"
-url2="amd-accelerated-parallel-processing-app-sdk/"
+url2="amd-accelerated-parallel-processing-app-sdk/download-archive/"
 url=$url1$url2
 
 # Lookup the JavaScript value of the of the download on the AMD SDK page
@@ -21,13 +21,12 @@ if [ $kernel == "x86_64" ] # 64-bit version
 then  
     echo "64-bit kernel detected"
     value1=`wget -qO - $url | sed -n '/download-2/,/64-bit/p'`
-    value2='QU1ELUFQUC1TREstdjIuOS1sbng2NC50Z3o='
- 
 else # 32-bit version
     echo "32-bit kernel detected"
     value1=`wget -qO - $url | sed -n '/download-1/,/32-bit/p'`
-    value2='QU1ELUFQUC1TREstdjIuOS1sbngzMi50Z3o='
 fi
+
+value2='QU1ELUFQUC1TREstdjIuOC4xLjAtbG54MzIudGd6'
 
 value1=`echo $value1 | awk -F 'value="' '{print $2}'`
 value1=`echo $value1 | awk -F'"' '{print $1}'`
@@ -40,7 +39,7 @@ text=$a$value1$b$c$value2
 wget --content-disposition --trust-server-names --post-data=$text $url
 
 
-tar zxvf AMD-APP-SDK-v2.9-lnx32.tgz 
+tar zxvf AMD-APP-SDK-*.tgz 
 sh ./Install-AMD-APP.sh 
 ln -sf /opt/AMDAPP/include/CL /usr/include
 ln -sf /opt/AMDAPP/lib/x86/* /usr/lib/
